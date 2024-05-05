0e2cd49aba903a570c6167ad8b83aeb43cb7eecf
lolisugar <wnyaiakashgsus@gmail.com>
2024-05-05 18:27:52 +0800

    2-7 增加hello接口测试 test.http

--- src/main/java/com/aiit/wiki/controlller/TestController.java
+++ src/main/java/com/aiit/wiki/controlller/TestController.java
@@ -1,13 +1,19 @@
 package com.aiit.wiki.controlller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello yuri";
+    }
+
+    @PostMapping("hi")
+    public String hi(String name){
+        return "hi"+"\t"+name;
     }
 }
