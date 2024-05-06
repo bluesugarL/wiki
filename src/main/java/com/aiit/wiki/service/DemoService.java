042034f86ab11275779f597f1d3034bb65506e6c
lolisugar <wnyaiakashgsus@gmail.com>
2024-05-06 15:25:39 +0800

    3-2 新增demo列表查询

---
+++ src/main/java/com/aiit/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.aiit.wiki.service;
+
+import com.aiit.wiki.domain.Demo;
+import com.aiit.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
