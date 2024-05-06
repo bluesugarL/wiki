66fff4ab3fe50524a74f63ac134017d36e13ffec
lolisugar <wnyaiakashgsus@gmail.com>
2024-05-06 21:42:33 +0800

    3-4 完成电子书列表查询接口

---
+++ src/main/java/com/aiit/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.aiit.wiki.service;
+
+import com.aiit.wiki.domain.Ebook;
+import com.aiit.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
