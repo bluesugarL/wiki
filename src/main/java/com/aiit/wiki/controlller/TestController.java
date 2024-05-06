c878eb6c58c50644d76c88ee01126de82ca8a0a3
lolisugar <wnyaiakashgsus@gmail.com>
2024-05-06 09:54:17 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/aiit/wiki/controlller/TestController.java
+++ src/main/java/com/aiit/wiki/controlller/TestController.java
@@ -1,5 +1,6 @@
 package com.aiit.wiki.controlller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:NULL}")
+    private String findNp;
+
     @GetMapping("/hello")
     public String hello(){
-        return "hello yuri";
+        return "hello"+"\t"+findNp;
     }
 
     @PostMapping("hi")
