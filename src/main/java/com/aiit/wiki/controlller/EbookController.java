52c84fc1b809a8a3f5269ddee24c6e2daa798c01
lolisugar <wnyaiakashgsus@gmail.com>
2024-05-06 22:38:09 +0800

    3-5 封装请求参数和返回参数

--- src/main/java/com/aiit/wiki/controlller/EbookController.java
+++ src/main/java/com/aiit/wiki/controlller/EbookController.java
@@ -1,7 +1,8 @@
 package com.aiit.wiki.controlller;
 
-import com.aiit.wiki.domain.Ebook;
+import com.aiit.wiki.req.EbookReq;
 import com.aiit.wiki.resp.CommonResp;
+import com.aiit.wiki.resp.EbookResp;
 import com.aiit.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq ebookReq) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(ebookReq);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/aiit/wiki/service/EbookService.java
+++ src/main/java/com/aiit/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.aiit.wiki.service;
 
 import com.aiit.wiki.domain.Ebook;
+import com.aiit.wiki.domain.EbookExample;
 import com.aiit.wiki.mapper.EbookMapper;
+import com.aiit.wiki.req.EbookReq;
+import com.aiit.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,22 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq ebookReq){
+        EbookExample ebookExample = new EbookExample();
+        //创建内部类
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+ebookReq.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        //创建一个泛型参数为EbookResp集合
+        List<EbookResp> respList=new ArrayList<>();
+        //遍历查询完结果的集合ebookList
+        for (Ebook ebook : ebookList) {
+            //创建符合respList集合泛型参参数的对象
+            EbookResp ebookResp = new EbookResp();
+            //完成对象的复制
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
